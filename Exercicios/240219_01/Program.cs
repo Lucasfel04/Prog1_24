9b062165b33c9fe34d266c80fd19241daa34ca07
Lucas Felipe Gonçalves de Morais <200976@unoesc.lan>
2024-03-04 19:17:27 -0300

    Primeiro commit

--- Exercicios/240219_01/Program.cs
+++ Exercicios/240219_01/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string message1 = null;
+
+string message2 = null;
+
+string message3 = System.String.Empty;
+
+var message4 = "Mensagem qualquer ";
+
+Console.WriteLine(message4);
+
+string concat  = (message1 == null ? "" : message1) + "" +  message2  + " "+  message3 + " " +  message4;
+
+Console.WriteLine("\n" + concat);
+
+Console.WriteLine("A Temperatura de hoje {0: D} é {1}°C", DateTime.Now, 23);
+
+string name = string.Empty;
+
+Console.WriteLine("Qual é o seu nome? ");
+name = Console.ReadLine();
+string resultado = $"Oi, {name} ! Pare de malandragem !";
+Console.WriteLine(resultado);
+
