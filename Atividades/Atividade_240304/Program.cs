ab02a1cfb1df0aeb2e0a1f98697cb88be60926fe
Lucas Felipe Gonçalves de Morais <200976@unoesc.lan>
2024-03-11 19:28:42 -0300

    Tiposnuméricos

---
+++ Atividades/Atividade_240304/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO.Pipes;
+using System.Runtime.Serialization.Formatters;
+
+string soma;
+string subtraia;
+string multiplica;
+string divida;
+string mensage1;
+
+int digitos = 0;
+
+mensage1 = "Qual tipo de conta quer faze-lo? ";
+
+
+for(digitos = 1; digitos < 10; digitos ++){
+    Console.WriteLine(digitos);
+}
+
+Console.WriteLine(mensage1);
